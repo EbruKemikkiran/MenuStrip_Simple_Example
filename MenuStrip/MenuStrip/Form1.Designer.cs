c30df1af6b8c99f122cead51cda0d5517c737aaa
Ebru Kemikkiran <104608342+EbruKemikkiran@users.noreply.github.com>
2022-09-15 14:44:19 +0100

    Add files via upload

---
+++ MenuStrip/MenuStrip/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace MenuStrip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.actionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.historyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ultimateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comedyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hangover1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hangover2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fieldlerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dontLookUPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pianoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.maxMareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.trainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toolStripComboBox2 = new System.Windows.Forms.ToolStripComboBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.actionToolStripMenuItem,
+            this.dramToolStripMenuItem,
+            this.historyToolStripMenuItem,
+            this.comedyToolStripMenuItem,
+            this.trainToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(479, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // actionToolStripMenuItem
+            // 
+            this.actionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator1,
+            this.maxMareToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.ultimateToolStripMenuItem});
+            this.actionToolStripMenuItem.Name = "actionToolStripMenuItem";
+            this.actionToolStripMenuItem.Size = new System.Drawing.Size(66, 24);
+            this.actionToolStripMenuItem.Text = "Action";
+            this.actionToolStripMenuItem.Click += new System.EventHandler(this.actionToolStripMenuItem_Click);
+            // 
+            // dramToolStripMenuItem
+            // 
+            this.dramToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oneToolStripMenuItem,
+            this.dontLookUPToolStripMenuItem,
+            this.toolStripSeparator3});
+            this.dramToolStripMenuItem.Name = "dramToolStripMenuItem";
+            this.dramToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.dramToolStripMenuItem.Text = "Dram";
+            // 
+            // historyToolStripMenuItem
+            // 
+            this.historyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pianoToolStripMenuItem,
+            this.fieldlerToolStripMenuItem});
+            this.historyToolStripMenuItem.Name = "historyToolStripMenuItem";
+            this.historyToolStripMenuItem.Size = new System.Drawing.Size(70, 24);
+            this.historyToolStripMenuItem.Text = "History";
+            // 
+            // ultimateToolStripMenuItem
+            // 
+            this.ultimateToolStripMenuItem.Name = "ultimateToolStripMenuItem";
+            this.ultimateToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ultimateToolStripMenuItem.Text = "Ultimate";
+            // 
+            // comedyToolStripMenuItem
+            // 
+            this.comedyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hangover1ToolStripMenuItem,
+            this.hangover2ToolStripMenuItem});
+            this.comedyToolStripMenuItem.Name = "comedyToolStripMenuItem";
+            this.comedyToolStripMenuItem.Size = new System.Drawing.Size(78, 24);
+            this.comedyToolStripMenuItem.Text = "Comedy";
+            // 
+            // hangover1ToolStripMenuItem
+            // 
+            this.hangover1ToolStripMenuItem.Name = "hangover1ToolStripMenuItem";
+            this.hangover1ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.hangover1ToolStripMenuItem.Text = "Hangover1";
+            // 
+            // hangover2ToolStripMenuItem
+            // 
+            this.hangover2ToolStripMenuItem.Name = "hangover2ToolStripMenuItem";
+            this.hangover2ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.hangover2ToolStripMenuItem.Text = "Hangover2";
+            // 
+            // fieldlerToolStripMenuItem
+            // 
+            this.fieldlerToolStripMenuItem.Name = "fieldlerToolStripMenuItem";
+            this.fieldlerToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.fieldlerToolStripMenuItem.Text = "Fieldler";
+            // 
+            // dontLookUPToolStripMenuItem
+            // 
+            this.dontLookUPToolStripMenuItem.Name = "dontLookUPToolStripMenuItem";
+            this.dontLookUPToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.dontLookUPToolStripMenuItem.Text = "Don\'t LookUP";
+            // 
+            // pianoToolStripMenuItem
+            // 
+            this.pianoToolStripMenuItem.Name = "pianoToolStripMenuItem";
+            this.pianoToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.pianoToolStripMenuItem.Text = "Piano";
+            // 
+            // oneToolStripMenuItem
+            // 
+            this.oneToolStripMenuItem.Name = "oneToolStripMenuItem";
+            this.oneToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.oneToolStripMenuItem.Text = "One";
+            // 
+            // maxMareToolStripMenuItem
+            // 
+            this.maxMareToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox2});
+            this.maxMareToolStripMenuItem.Name = "maxMareToolStripMenuItem";
+            this.maxMareToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.maxMareToolStripMenuItem.Text = "MaxMare";
+            this.maxMareToolStripMenuItem.Click += new System.EventHandler(this.maxMareToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(221, 6);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(221, 6);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(221, 6);
+            // 
+            // trainToolStripMenuItem
+            // 
+            this.trainToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1});
+            this.trainToolStripMenuItem.Name = "trainToolStripMenuItem";
+            this.trainToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.trainToolStripMenuItem.Text = "rank";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Items.AddRange(new object[] {
+            "10",
+            "9",
+            "8",
+            "7",
+            "6",
+            "5",
+            "4",
+            "3",
+            "2",
+            "1",
+            "0"});
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 28);
+            this.toolStripComboBox1.Click += new System.EventHandler(this.toolStripComboBox1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 140);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(351, 332);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 98);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(288, 36);
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(306, 98);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(57, 36);
+            this.listBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Film";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(306, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Rank";
+            // 
+            // toolStripComboBox2
+            // 
+            this.toolStripComboBox2.Name = "toolStripComboBox2";
+            this.toolStripComboBox2.Size = new System.Drawing.Size(121, 28);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 611);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem actionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem maxMareToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem ultimateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dramToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oneToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dontLookUPToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem historyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pianoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fieldlerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem comedyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hangover1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hangover2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trainToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox2;
+    }
+}
+
---
+++ MenuStrip/MenuStrip/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MenuStrip
+{
+    public partial class Form1 : Form
+    {
+        private const string V = "Lorem Ipsum";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void maxMareToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripComboBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private ListBox GetListBox1()
+        {
+            return listBox1;
+        }
+
+        private void actionToolStripMenuItem_Click(object sender, EventArgs e, int v, int v1, int _, ListBox listBox1, int v, int v)
+        {
+            _ = _ = listBox1.Items.Add("MaxMare");
+            richTextBox1.Text = V;
+            listBox2.Items.Add(toolStripComboBox1.Text);
+
+
+        }
+    }
+}
